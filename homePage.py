5292368dba70bc45803340a67336309563b1b3e3
Autumn Benton <78234827+AutumnSageBenton@users.noreply.github.com>
2021-12-01 17:32:26 -0800

    Add files via upload

---
+++ homePage.py
@@ -0,0 +1,78 @@
+# Final Project: CST 205
+# Team 798
+# Members:
+# #   Hasem Trejo
+# #   Justin Ho
+# #   Jaden Bussard
+# #   Autumn Benton
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from pagesInfo import info
+import random
+from PIL import Image
+import math
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+
+@app.route('/')
+def home():
+    #random.shuffle(info)
+    title = info[0][2]
+    temp = info[0][3]
+    return render_template('homeTemplate.html', name=title)
+
+
+@app.route('/page1/')
+def page1():
+    myImage = Image.open('static/jelly.jpg')
+    background = Image.open("static/csumb.jpg")
+    negatives(myImage)
+    greys(myImage)
+
+    return render_template('p1Template.html')
+
+
+
+@app.route('/page2/')
+def page2():
+    
+    return render_template('p2Template.html')
+
+
+@app.route('/page3/')
+def page3():
+
+    return render_template('p3Template.html')
+
+
+@app.route('/page4/')
+def page4():
+    
+    return render_template('p4Template.html')
+
+
+
+
+def sepia(im):
+    new_list=[]
+    for p in im.getdata():
+        new_list.append((p[0], p[1]//2, p[2]//2))
+        im.putdata(new_list)
+    im.save('static/imSepia.jpg')
+
+def negatives(im):      
+    negative_list = [(255-p[0], 255-p[1], 255-p[2]) for p in im.getdata()]
+    im.putdata(negative_list)
+    im.save('static/imNegative.jpg')
+
+def greys(im):
+    new_list = [( (a[0]+a[1]+a[2])//3, ) * 3 for a in im.getdata() ]
+    im.putdata(new_list)
+    im.save('static/imGrey.jpg')
+
+
+
---
+++ pagesInfo.py
@@ -0,0 +1,6 @@
+info = [
+    [1, 'Page 1', 'page1', 'p1Template.html'],
+    [2, 'Page 2', 'page2', 'p2Template.html'],
+    [3, 'Page 3', 'page3', 'p3Template.html'], 
+    [4, 'Page 4', 'page4', 'p3Template.html']
+]
